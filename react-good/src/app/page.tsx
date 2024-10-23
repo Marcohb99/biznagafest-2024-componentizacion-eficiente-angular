2c726e29023efc114e366045c71a02bf1ecb224d
ManuRGDev <manurgdev@gmail.com>
2024-10-23 19:42:01 +0200

    feat: add TodoListWithHandling component

--- react-good/src/app/page.tsx
+++ react-good/src/app/page.tsx
@@ -1,12 +1,14 @@
-import { TodoList } from "@/scenes/TodoList";
-// import { TodoListWithLogic } from "@/scenes/TodoList";
+// import { TodoList } from "@/scenes/TodoList/TodoList";
+// import { TodoListWithLogic } from "@/scenes/TodoList/TodoListWithLogic";
+import { TodoListWithHandling } from "@/scenes/TodoList/TodoListWithHandling";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full bg-gray-100 flex">
       <div className="w-full p-4">
-        <TodoList />
+        {/* <TodoList /> */}
         {/* <TodoListWithLogic /> */}
+        <TodoListWithHandling />
       </div>
     </div>
   );
