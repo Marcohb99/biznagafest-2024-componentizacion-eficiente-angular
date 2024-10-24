9bf274622550491b83ac259af68d9bd07986b583
ManuRGDev <manurgdev@gmail.com>
2024-10-24 08:41:06 +0200

    feat: add TodoInput component

---
+++ vue-regular/src/components/TodoInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <input
+    :value="newTodo"
+    @input="$emit('update:new-todo', $event.target.value)"
+    @keyup.enter="$emit('add-todo')"
+    placeholder="Añadir nueva tarea..."
+    class="w-4/6 md:w-5/6 p-2 rounded-lg bg-white border border-gray-300"
+  />
+  <button
+    @click="$emit('add-todo')"
+    class="w-2/6 md:w-1/6 p-2.5 md:p-2 rounded-lg bg-blue-500 text-white"
+  >
+    Añadir
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'TodoInput',
+  props: {
+    newTodo: String,
+  },
+};
+</script>
